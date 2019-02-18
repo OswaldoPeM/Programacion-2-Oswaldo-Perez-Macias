e3f09d45b644e7ba5a9ffd1a8bab54b4642f6125
OswaldoPeM <39318131+OswaldoPeM@users.noreply.github.com>
2019-02-17 22:26:31 -0600

    es par?

---
+++ Practicas 5 es par/Source .cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <string>
+bool isPair(char z) {
+	switch (z)
+	{
+	case'2':case'4':case'6':case'8':case'0':
+		return true;
+	default:
+		return false;
+		break;
+	}
+}
+int main() {
+	std::string valor;
+	while (valor!="x")
+	{
+	std::cout << "Introdusca numero para saber si es par\nPara cerrar precione x\n";
+	std::cin >> valor;
+	(isPair(valor[valor.size()-1])) ? std::cout << "\nEs par\n" : std::cout << "\nNo es par\n";
+	}
+	return 0;
+}
