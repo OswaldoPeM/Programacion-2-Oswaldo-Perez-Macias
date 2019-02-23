03bc4fa8e143cf7d92eb75912affbbf234ccddc5
OswaldoPeM <39318131+OswaldoPeM@users.noreply.github.com>
2019-02-22 20:54:51 -0600

    Add files via upload

---
+++ Practicas 7 SumOfSequence/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+//si existe una una manera de llegar de star a end con aumentos de step, te devuelve una suma de todos esos pasos incluyendo start y end. si no es posible devuelve un 0
+int sequenceSum(int start, int end, int step)
+{
+	if (start > end) return 0;
+
+	int loops = (end - start) / step;
+	return start * (loops + 1) + step * (loops * loops + loops) / 2;
+}
+
+int main() {
+	std::string ciclo = "";
+	int start, end, step;
+
+	while (ciclo != "x")
+	{
+		std::cout << "introdusca start, end y step en ese orden\n";
+		std::cin >> start; std::cin >> end; std::cin >> step;
+
+		std::cout << "\nEl resultado es: " << sequenceSum(start, end, step) << "\nsi quiere salir del programa presione 'x', si quiere seguir jugando introdusca cualquier caracter\n";
+		std::cin >> ciclo;
+	}
+	return 0;
+}
